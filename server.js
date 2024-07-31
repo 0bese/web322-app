6a0c8c426a221c97c1ce253fa0b8352e7499c16d
0bese <134105909+0bese@users.noreply.github.com>
2024-07-31 10:09:26 -0400

    protected routes

--- server.js
+++ server.js
@@ -11,12 +11,12 @@
  *  Published URL: https://web322mylegoapp.vercel.app/
  *
  ********************************************************************************/
-"use server";
 const express = require("express");
 const legoData = require("./modules/legoSets");
 const path = require("path");
 const authData = require("./modules/auth-service.js");
 const app = express();
+const clientSessions = require("client-sessions");
 
 const API_PORT = process.env.PORT || 8000;
 
@@ -26,16 +26,107 @@ const year = new Date().getFullYear();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-// legoData.initialize()
-//   .then(authData.initialize)
-//   .then(() => {
-//     app.listen(HTTP_PORT, () => {
-//       console.log(`app listening on: ${HTTP_PORT}`);
-//     });
-//   })
-//   .catch(err => {
-//     console.log(`unable to start server: ${err}`);
-//   });
+app.use(
+  clientSessions({
+    cookieName: "session",
+    secret: process.env.SESSION_SECRET,
+    duration: 15 * 60 * 1000,
+    activeDuration: 1000 * 60,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+function ensureLogin(req, res, next) {
+  if (!req.session) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
+legoData
+  .initialize()
+  .then(authData.initialize)
+  .then(() => {
+    app.listen(API_PORT, () => {
+      console.log(`app listening on: ${API_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`unable to start server: ${err}`);
+  });
+
+//authentication
+
+app.get("/login", async (req, res) => {
+  await res.render("login", {
+    errorMessage: null,
+    year: year,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  req.body.userAgent = req.get("User-Agent");
+  try {
+    const user = await authData.checkUser(req.body);
+    if (user) {
+      // Add user details to session
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory,
+      };
+      res.redirect("/lego/sets");
+    } else {
+      res.render("login", {
+        errorMessage: "Invalid username or password",
+      });
+    }
+  } catch (err) {
+    res.render("login", { errorMessage: err });
+  }
+});
+
+app.get("/register", async (req, res) => {
+  await res.render("register", {
+    errorMessage: null,
+    successMessage: null,
+    year: year,
+  });
+});
+
+// Route for handling user registration
+app.post("/register", async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const newUser = await authData.registerUser(req.body);
+    res.render("register", {
+      successMessage: "User created",
+      errorMessage: null,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("register", {
+      errorMessage: err,
+      userName,
+      successMessage: null,
+    });
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.session.reset();
+  res.redirect("/");
+});
+
+// Route for user history
+app.get("/userHistory", ensureLogin, (req, res) => {
+  res.render("userHistory");
+});
 
 //routes
 app.get("/", async (req, res) => {
@@ -46,22 +137,7 @@ app.get("/about", async (req, res) => {
   await res.render("about");
 });
 
-app.get("/login", async (req, res) => {
-  await res.render("login", {
-    userName: "somtin",
-    errorMessage: "yawa dey",
-    year: year,
-  });
-});
-app.get("/register", async (req, res) => {
-  await res.render("register", {
-    userName: "somtin",
-    errorMessage: "yawa dey",
-    year: year,
-  });
-});
-
-app.get("/lego/sets", async (req, res) => {
+app.get("/lego/sets", ensureLogin, async (req, res) => {
   try {
     if (req.query.theme) {
       let sets = await legoData.getSetsByTheme(req.query.theme);
@@ -77,7 +153,7 @@ app.get("/lego/sets", async (req, res) => {
   }
 });
 
-app.get("/lego/sets/:num", async (req, res) => {
+app.get("/lego/sets/:num", ensureLogin, async (req, res) => {
   try {
     let set = await legoData.getSetByNum(req.params.num);
     res.render("set", { legoSet: set });
@@ -90,7 +166,7 @@ app.get("/lego/sets/:num", async (req, res) => {
 
 // start
 
-app.get("/lego/addSet", async (req, res) => {
+app.get("/lego/addSet", ensureLogin, async (req, res) => {
   try {
     let themeData = await legoData.getAllThemes();
     res.render("addSet", { themes: themeData });
@@ -102,7 +178,7 @@ app.get("/lego/addSet", async (req, res) => {
 });
 
 // POST route to handle form submission
-app.post("/lego/addSet", async (req, res) => {
+app.post("/lego/addSet", ensureLogin, async (req, res) => {
   try {
     await legoData.addSet(req.body);
     res.redirect("/lego/sets");
@@ -113,7 +189,7 @@ app.post("/lego/addSet", async (req, res) => {
   }
 });
 
-app.get("/lego/editSet/:num", async (req, res) => {
+app.get("/lego/editSet/:num", ensureLogin, async (req, res) => {
   try {
     const set = await legoData.getSetByNum(req.params.num);
     const themes = await legoData.getAllThemes();
@@ -124,7 +200,7 @@ app.get("/lego/editSet/:num", async (req, res) => {
 });
 
 // POST route to handle form submission for editing a set
-app.post("/lego/editSet", async (req, res) => {
+app.post("/lego/editSet", ensureLogin, async (req, res) => {
   try {
     const setNum = req.body.set_num;
     const setData = {
@@ -143,7 +219,7 @@ app.post("/lego/editSet", async (req, res) => {
   }
 });
 
-app.get("/lego/deleteSet/:num", async (req, res) => {
+app.get("/lego/deleteSet/:num", ensureLogin, async (req, res) => {
   try {
     await legoData.deleteSet(req.params.num);
     res.redirect("/lego/sets");
@@ -159,11 +235,5 @@ app.get("/lego/deleteSet/:num", async (req, res) => {
 app.use((req, res, next) => {
   res.status(404).render("404", {
     message: "I'm sorry, we're unable to find what you're looking for.",
-  });
-});
-
-legoData.initialize().then(() => {
-  app.listen(API_PORT, () => {
-    console.log(`server running on port ${API_PORT}`);
   });
 });
